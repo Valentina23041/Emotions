8f7647c86553551cc1c8bddc836205ba96daad74
Andreshur07 <adhurtado@unimayor.edu.co>
2026-05-22 22:51:58 -0500

    Configura Firebase con variable de entorno

--- emotions_app/emotions_app/infrastructure/firebase_config.py
+++ emotions_app/emotions_app/infrastructure/firebase_config.py
@@ -1,8 +1,17 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    firebase_json = os.environ.get("FIREBASE_CONFIG")
+
+    if firebase_json:
+        firebase_config = json.loads(firebase_json)
+        cred = credentials.Certificate(firebase_config)
+    else:
+        cred = credentials.Certificate("firebase_key.json")
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
